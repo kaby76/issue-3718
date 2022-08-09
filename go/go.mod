6a998f23db0fd8cfefc66855ec3000fa98cc082a
Ken Domino <kenneth.domino@domemtech.com>
2022-08-09 07:17:18 -0400

    Updates.

--- go/go.mod
+++ go/go.mod
@@ -2,4 +2,4 @@ module example.com/myparser
 
 go 1.17
 
-require github.com/antlr/antlr4/runtime/Go/antlr v0.0.0-20220411172017-c7567ba9bb40 // indirect
+require github.com/antlr/antlr4/runtime/Go/antlr v0.0.0-20220415214452-44d87bc1d130
--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pushd csharp; make; ./bin/Debug/net6.0/Test.exe -file ../p5.txt > out.txt 2>&1; dos2unix out.txt; popd
-pushd go; make; ./Test.exe -file ../p5.txt > out.txt 2>&1; dos2unix out.txt; popd
+pushd go; rm Test.exe; make; ./Test.exe -file ../p5.txt > out.txt 2>&1; dos2unix out.txt; popd
